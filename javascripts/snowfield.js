0c21d906d25aff1bd9390abb4389c8a7d41f188d
HughG <githugh@tameter.org>
2012-12-25 21:54:06 +0000

    Minimal snowflake

--- javascripts/snowfield.js
+++ javascripts/snowfield.js
@@ -1,1 +1,19 @@
-/* Placeholder */
+(function scope() (
+
+var origin = new Point();
+
+var spoke = new Point();
+spoke.length = 100;
+spoke.angle = 0;
+
+var spokePath;
+var spokeCount = 6;
+var i;
+for (i = 0; i < spokeCount; i++) {
+  spokePath = new Path();
+  spokePath.add(origin);
+  spokePath.add(origin + spoke);
+  spoke.angle += (360 / spokeCount);
+}
+
+))();
