6b3503d9a388af943fb1a845474f53b401856c7a
Hugh Greene <github@tameter.org>
2012-12-27 13:52:44 +0000

    Add cross-pieces
    Added code to draw some cross-pieces across each spine.

--- javascripts/snowfield.js
+++ javascripts/snowfield.js
@@ -1,6 +1,6 @@
 (function () {
 
-// Seedable pseudo-random number generator, from http://stackoverflow.com/a/424445
+// Seedable pseudo-random number generator, after http://stackoverflow.com/a/424445
 function RNG(seed) {
   // LCG using GCC's constants
   this.m = 0x100000000; // 2**32;
@@ -24,6 +24,13 @@ RNG.prototype.nextRange = function(start, end) {
   var randomUnder1 = this.nextInt() / this.m;
   return start + Math.floor(randomUnder1 * rangeSize);
 }
+RNG.prototype.nextFloatRange = function(start, end) {
+  // returns in range [start, end): including start, excluding end
+  // can't modulu nextInt because of weak randomness in lower bits
+  var rangeSize = end - start;
+  var randomUnder1 = this.nextInt() / this.m;
+  return start + (randomUnder1 * rangeSize);
+}
 RNG.prototype.choice = function(array) {
   return array[this.nextRange(0, array.length)];
 }
@@ -36,15 +43,45 @@ function drawScene () {
   paper.setup(canvas);
 
   // Seed the RNG.
-  var rng = new RNG(20);
+  //var rng = new RNG(20);
+  var rng = new RNG(Math.random() * 100);
 
   // BEGIN Helper functions ----------------------------------------
-  function drawSpoke (origin, spokeEnd) {
+  function drawSpoke (origin, spokeEnd, crossParams) {
     var spokePath = new paper.Path();
     spokePath.strokeColor = '#d0e8ff';
     spokePath.strokeWidth = 3;
     spokePath.add(origin);
     spokePath.add(origin.add(spokeEnd));
+
+    var crossStartDist = rng.nextFloatRange(0.8, 1);
+    var crossEndDist = rng.nextFloatRange(0.05, 0.2);
+    var crossSpacingFactor = rng.nextFloatRange(0.6, 0.9);
+    var crossSpikinessFactor = rng.nextFloatRange(0.6, 1.3);
+    var crossAngle = rng.nextRange(10, 45);
+
+    var c;
+    for (
+        c = crossParams.startDist;
+        c > crossParams.endDist;
+        c *= crossParams.spacingFactor
+    ) {
+      var crossPoint = spokeEnd.clone();
+      crossPoint.length *= c;
+      var crossWiddershins = crossPoint.clone();
+      crossWiddershins.angle -= crossParams.spreadAngle;
+      crossWiddershins.length *= crossParams.spikinessFactor;
+      var crossTurnwise = crossPoint.clone();
+      crossTurnwise.angle += crossParams.spreadAngle;
+      crossTurnwise.length *= crossParams.spikinessFactor;
+
+      var crossPath = new paper.Path();
+      crossPath.strokeColor = '#d0e8ff';
+      crossPath.strokeWidth = 2;
+      crossPath.add(origin.add(crossWiddershins));
+      crossPath.add(origin.add(crossPoint));
+      crossPath.add(origin.add(crossTurnwise));
+    }
   }
 
   function drawSnowflake (origin) {
@@ -53,9 +90,17 @@ function drawScene () {
     spokeEnd.angle = 0;
 
     var spokeCount = 2 * Math.floor(rng.nextRange(3, 7));
+    var crossParams = {
+      startDist: rng.nextFloatRange(0.8, 1),
+      endDist: rng.nextFloatRange(0.05, 0.2),
+      spacingFactor: rng.nextFloatRange(0.6, 0.9),
+      spikinessFactor: rng.nextFloatRange(0.6, 1.3),
+      spreadAngle: rng.nextRange(10, 35)
+    };
+
     var i;
     for (i = 0; i < spokeCount; i++) {
-      drawSpoke(origin, spokeEnd);
+      drawSpoke(origin, spokeEnd, crossParams);
       spokeEnd.angle += (360 / spokeCount);
     }
   }
